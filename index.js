024e9de776bab7bcaa6caa8e26d9be1e721f232e
Vova <vova77gh@gmail.com>
2020-01-05 15:54:54 +0200

    include: body-parser; processing when form submitted; POST

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.set('view engine', 'ejs')   //default: search .ejs in /views/...
 app.use(express.static('public'))   //static files in /public/...
+app.use(bodyParser.urlencoded({extended: true}))   //need for 'req.body' to read data
 
 
 // When I reload page(request to root page), then 'get' request is begin:
 app.get('/', (req, res) => {
     res.render('index')     // and file index.ejs will be render
+})
+
+
+app.post('/', (req, res) => {  //res -- it is answear, req -- request
+    
+    const { city } = req.body
+    console.log(city)
+
+    res.render('index')
 })
 
 
